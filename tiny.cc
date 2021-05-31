da667f1a778f8aafa684bf122854a3d3cc992bf8
Jing LIU <jing.liu@usd.edu>
2021-05-31 07:43:39 -0500

    reformatted

--- tiny.cc
+++ tiny.cc
@@ -1,2 +1,7 @@
 #include "G4UIExecutive.hh"
-int main(int argc,char** argv) { G4UIExecutive ui(argc, argv); ui.SessionStart(); }
+
+int main(int argc,char** argv)
+{
+	G4UIExecutive ui(argc, argv);
+	ui.SessionStart();
+}
