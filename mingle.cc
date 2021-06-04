bd10ff8e9673232f0053ad7a98b0a1a63ff718f0
Jing LIU <jing.liu@usd.edu>
2021-06-04 11:40:07 -0400

    added batch mode

--- mingle.cc
+++ mingle.cc
@@ -1,7 +1,13 @@
-#include "G4UIExecutive.hh"
+#include <G4UIExecutive.hh>
+#include <G4UImanager.hh>
 
 int main(int argc,char** argv)
 {
-	G4UIExecutive ui(argc, argv);
-	ui.SessionStart();
+	if (argc==1) { // interactive mode
+		G4UIExecutive ui(argc, argv);
+		ui.SessionStart();
+	} else { // batch mode
+		G4String command = "/control/execute ";
+		G4UImanager::GetUIpointer()->ApplyCommand(command+argv[1]);
+	}
 }
