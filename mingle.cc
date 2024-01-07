a0e515de04467ce21ba2df2d96ec672807d7ec90
Jing Liu <jintonic@physino.xyz>
2024-01-07 09:24:33 -0600

    removed analysis manager from mingle.cc

--- mingle.cc
+++ mingle.cc
@@ -1,7 +1,5 @@
 #include <G4VUserDetectorConstruction.hh>
-#include <G4PSEnergyDeposit.hh>
 #include <G4tgbVolumeMgr.hh>
-#include <G4SDManager.hh>
 
 class Detector : public G4VUserDetectorConstruction
 {
@@ -10,47 +8,9 @@ class Detector : public G4VUserDetectorConstruction
 			G4tgbVolumeMgr::GetInstance()->AddTextFile("detector.tg");
 			return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
 		} ///< load detector definition from a text file "detector.tg"
-		void ConstructSDandField() {
-			G4MultiFunctionalDetector* d = new G4MultiFunctionalDetector("d");
-			G4SDManager::GetSDMpointer()->AddNewDetector(d);
-			G4VPrimitiveScorer* scorer = new G4PSEnergyDeposit("e");
-			d->RegisterPrimitive(scorer);
-			SetSensitiveDetector("d",d);
-		} ///< make "d" volume in "detector.tg" sensitive
 };
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-#include <G4UserRunAction.hh>
-#include <G4TScoreHistFiller.hh>
-#include <G4TScoreNtupleWriter.hh>
-#include <G4GenericAnalysisManager.hh>
 
-class RunAction : public G4UserRunAction
-{
-	public:
-		G4GenericAnalysisManager *fAnaMgr;
-		G4TScoreHistFiller<G4GenericAnalysisManager> *fFiller;
-		G4TScoreNtupleWriter<G4GenericAnalysisManager> *fWriter;
-
-		RunAction() : G4UserRunAction() {
-			fAnaMgr = G4GenericAnalysisManager::Instance();
-			fAnaMgr->SetNtupleMerging(true); // merge data from multiple threads
-			fFiller = new G4TScoreHistFiller<G4GenericAnalysisManager>;
-			fWriter = new G4TScoreNtupleWriter<G4GenericAnalysisManager>;
-		} ///< initialize analysis manager, histogram filler and ntuple writer
-		~RunAction() { delete fFiller; delete fWriter; }
-
-		void BeginOfRunAction(const G4Run*) {
-			if (fAnaMgr->GetFileName()=="") return;
-			fAnaMgr->OpenFile(fAnaMgr->GetFileName());
-		} ///< open output file specified by /analysis/setFileName
-		void EndOfRunAction(const G4Run*) {
-			if (fAnaMgr->GetFileName()=="") return;
-			fAnaMgr->Write(); fAnaMgr->CloseFile();
-		} ///< write data to and close output file if it is specified
-};
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #include <G4VUserPrimaryGeneratorAction.hh>
 #include <G4GeneralParticleSource.hh>
 
@@ -64,23 +24,21 @@ class Generator : public G4VUserPrimaryGeneratorAction
 		~Generator() { delete fGPS; }
 		void GeneratePrimaries(G4Event *evt) { fGPS->GeneratePrimaryVertex(evt); }
 };
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 #include <G4VUserActionInitialization.hh>
 
 class Action : public G4VUserActionInitialization
 {
 	public:
-		void BuildForMaster() const { SetUserAction(new RunAction); }
-		void Build() const {
-			SetUserAction(new RunAction);
-			SetUserAction(new Generator);
-		}
+		void Build() const { SetUserAction(new Generator); }
 };
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include <G4TScoreNtupleWriter.hh>
 #include <G4RunManagerFactory.hh>
 #include <G4PhysListFactory.hh>
+#include <G4AnalysisManager.hh>
 #include <G4ScoringManager.hh>
 #include <G4VisExecutive.hh>
 #include <G4UIExecutive.hh>
@@ -91,7 +49,11 @@ int main(int argc,char** argv)
 {
 	auto *run = G4RunManagerFactory::CreateRunManager();
 
-	G4ScoringManager::GetScoringManager();
+	G4ScoringManager::GetScoringManager(); // enable macro commands in /score/
+
+	// https://geant4-forum.web.cern.ch/t/11480/3
+	G4TScoreNtupleWriter<G4AnalysisManager> writer;
+	writer.SetNtupleMerging(true); // merge ntuples created in different threads
 
 	G4PhysListFactory factory;
 	run->SetUserInitialization(factory.ReferencePhysList());
