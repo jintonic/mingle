c8b9f67c686521fb76c6ccb953d729da906c35e4
Jing Liu <jintonic@physino.xyz>
2025-12-30 10:24:20 -0600

    reformated

--- mingle.cc
+++ mingle.cc
@@ -2,11 +2,11 @@
 #include <G4tgbVolumeMgr.hh>
 class Detector : public G4VUserDetectorConstruction
 {
-	public:
-		G4VPhysicalVolume* Construct() {
-			G4tgbVolumeMgr::GetInstance()->AddTextFile("detector.tg");
-			return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
-		} ///< load detector definition from a text file "detector.tg"
+  public:
+	G4VPhysicalVolume* Construct() {
+	  G4tgbVolumeMgr::GetInstance()->AddTextFile("detector.tg");
+	  return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
+	} ///< load detector definition from a text file "detector.tg"
 };
 
 #include <G4RunManagerFactory.hh>
@@ -15,7 +15,7 @@ class Detector : public G4VUserDetectorConstruction
 #include <G4UImanager.hh>
 int main(int argc, char** argv)
 {
-  auto *run = G4RunManagerFactory::CreateRunManager();
+  auto run = G4RunManagerFactory::CreateRunManager();
 
   // load default physics list, or the one specified by $PHYSLIST
   G4PhysListFactory f; run->SetUserInitialization(f.ReferencePhysList());
