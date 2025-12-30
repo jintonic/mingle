c5c2a07722226e10223d5f5c05b14f1dc8e8152a
Jing Liu <jintonic@physino.xyz>
2025-12-30 10:27:55 -0600

    reformatted

--- mingle.cc
+++ mingle.cc
@@ -4,18 +4,18 @@
 #include <G4UImanager.hh>
 int main(int argc, char** argv)
 {
-	auto *run = G4RunManagerFactory::CreateRunManager();
+  auto *run = G4RunManagerFactory::CreateRunManager();
 
-	// load default physics list, or the one specified by $PHYSLIST
-	G4PhysListFactory f; run->SetUserInitialization(f.ReferencePhysList());
+  // load default physics list, or the one specified by $PHYSLIST
+  G4PhysListFactory f; run->SetUserInitialization(f.ReferencePhysList());
 
-	if (argc==1) { // interactive mode
-		G4UIExecutive ui(argc, argv);
-		ui.SessionStart();
-	} else { // batch mode
-		G4String command = "/control/execute ";
-		G4UImanager::GetUIpointer()->ApplyCommand(command+argv[1]);
-	}
+  if (argc == 1) { // interactive mode
+    G4UIExecutive ui(argc, argv);
+    ui.SessionStart();
+  } else { // batch mode
+    G4String command = "/control/execute ";
+    G4UImanager::GetUIpointer()->ApplyCommand(command + argv[1]);
+  }
 
-	delete run;
+  delete run;
 }
