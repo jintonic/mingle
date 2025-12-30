95fcd64868429156f85d08eed55a34208a721a57
Jing Liu <jintonic@physino.xyz>
2025-12-30 10:45:52 -0600

    tab -> spaces

--- mingle.cc
+++ mingle.cc
@@ -2,11 +2,11 @@
 #include <G4UImanager.hh>
 int main(int argc, char** argv)
 {
-	if (argc==1) { // interactive mode
-		G4UIExecutive ui(argc, argv);
-		ui.SessionStart();
-	} else { // batch mode
-		G4String command = "/control/execute ";
-		G4UImanager::GetUIpointer()->ApplyCommand(command+argv[1]);
-	}
+  if (argc == 1) { // interactive mode
+    G4UIExecutive ui(argc, argv);
+    ui.SessionStart();
+  } else { // batch mode
+    G4String command = "/control/execute ";
+    G4UImanager::GetUIpointer()->ApplyCommand(command + argv[1]);
+  }
 }
