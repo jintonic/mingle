fdf98a4c0ae11472d7fdade0732cba9cb4734238
Jing Liu <jintonic@physino.xyz>
2026-01-02 17:31:16 -0600

    minor change

--- mingle.cc
+++ mingle.cc
@@ -5,12 +5,12 @@ int main(int argc, char** argv)
 {
   auto run = G4RunManagerFactory::CreateRunManager();
 
-  if (argc == 1) { // interactive mode
+  if (argc == 1) { // start an interactive UI session
     G4UIExecutive ui(argc, argv);
     ui.SessionStart();
-  } else { // batch mode
-    G4String command = "/control/execute ";
-    G4UImanager::GetUIpointer()->ApplyCommand(command + argv[1]);
+  } else { // run a macro file in batch mode
+    G4String cmd = "/control/execute ", macroFile = argv[1];
+    G4UImanager::GetUIpointer()->ApplyCommand(cmd + macroFile);
   }
 
   delete run;
