8a8058566f13c7a4232ea627637276a141059bff
Jing Liu <jintonic@physino.xyz>
2026-01-03 20:20:27 -0600

    removed log scale plot

--- drawE.py
+++ drawE.py
@@ -1,38 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # open a root file using uproot
 import uproot
 f = uproot.open('scoring.root')
 # check what's in the file
 f.keys()
 
-# get ROOT tree (https://root.cern/manual/trees/) named 'CsI_e'
-t = f['CsI_e']
+# get ROOT tree (https://root.cern/manual/trees/) named 'Shape1_e'
+t = f['Shape1_e']
 # list branches in the tree
 t.keys()
-# get branch 'CsI_e_score'
-b = t['CsI_e_score']
+# get branch 'Shape1_e_score'
+b = t['Shape1_e_score']
 # get values in the branch as an awkward array
 e = b.array()
 
 # draw energy (e) distribution
 import matplotlib.pyplot as p
 p.hist(e, bins=100)
-p.xlabel('Energy deposition in CsI [MeV]')
-p.ylabel('Entries')
-p.title('Linear scale')
-p.text(1.63, 4020, r'Full-absorption peak $\rightarrow$')
-p.grid(True)
-
-# draw e in log scale
-p.figure()
-p.hist(e, bins=100, log=True)
-p.xlabel('Energy deposition in CsI [MeV]')
-p.ylabel('Entries')
-p.title('Log scale')
-p.text(1.18, 250, r'Single-escape peak $\rightarrow$')
-p.text(0.63, 6.2, r'Compton continuum', color='white')
+p.xlabel('Energy deposition in Shape1 [MeV]')
+p.ylabel('Entries/(6 keV)')
+p.title('Histogram Generated from Ntuple')
+p.text(0.038, 810, r'Full-absorption peak $\rightarrow$')
+p.text(0, 410, r'Back-scattering peak')
 p.grid(True)
 
 # show canvas
 p.show()
-
