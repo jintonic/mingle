09ebac04b714731faa736e890d091c4973f4d4dd
Jing Liu <jintonic@physino.xyz>
2026-01-03 18:22:13 -0600

    adopted vis version

--- mingle.cc
+++ mingle.cc
@@ -1,70 +1,56 @@
 #include <G4VUserDetectorConstruction.hh>
 #include <G4tgbVolumeMgr.hh>
-
 class Detector : public G4VUserDetectorConstruction
 {
-	public:
-		G4VPhysicalVolume* Construct() {
-			G4tgbVolumeMgr::GetInstance()->AddTextFile("detector.tg");
-			return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
-		} ///< load detector definition from a text file "detector.tg"
+  public:
+    G4VPhysicalVolume* Construct() {
+      G4tgbVolumeMgr::GetInstance()->AddTextFile("detector.tg");
+      return G4tgbVolumeMgr::GetInstance()->ReadAndConstructDetector();
+    } ///< load detector definition from a text file "detector.tg"
 };
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #include <G4VUserPrimaryGeneratorAction.hh>
 #include <G4GeneralParticleSource.hh>
-
 class Generator : public G4VUserPrimaryGeneratorAction
 {
-	private:
-		G4GeneralParticleSource* fGPS;
-	public:
-		Generator() : G4VUserPrimaryGeneratorAction() {
-			fGPS = new G4GeneralParticleSource; }
-		~Generator() { delete fGPS; }
-		void GeneratePrimaries(G4Event *evt) { fGPS->GeneratePrimaryVertex(evt); }
+  private:
+    G4GeneralParticleSource* fGPS;
+  public:
+    Generator() : G4VUserPrimaryGeneratorAction() { fGPS = new G4GeneralParticleSource; }
+    ~Generator() { delete fGPS; }
+    void GeneratePrimaries(G4Event *evt) { fGPS->GeneratePrimaryVertex(evt); }
 };
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #include <G4VUserActionInitialization.hh>
-
 class Action : public G4VUserActionInitialization
 {
-	public:
-		void Build() const { SetUserAction(new Generator); }
+  public:
+    void Build() const { SetUserAction(new Generator); }
 };
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #include <G4RunManagerFactory.hh>
 #include <G4PhysListFactory.hh>
-#include <G4ScoringManager.hh>
 #include <G4VisExecutive.hh>
 #include <G4UIExecutive.hh>
-#include <G4VisManager.hh>
 #include <G4UImanager.hh>
-
-int main(int argc,char** argv)
+int main(int argc, char** argv)
 {
-	auto *run = G4RunManagerFactory::CreateRunManager();
+  auto run = G4RunManagerFactory::CreateRunManager();
+  // load default physics list, or the one specified by $PHYSLIST
+  G4PhysListFactory f; run->SetUserInitialization(f.ReferencePhysList());
+  run->SetUserInitialization(new Detector); // load detector definition
+  run->SetUserInitialization(new Action); // load particle generator
 
-	G4ScoringManager::GetScoringManager();
+  G4UIExecutive* ui = nullptr; // assume batch mode by default
+  if (argc==1) ui = new G4UIExecutive(argc, argv); // interactive mode
+  // enable visialization between UI creation and session start
+  auto vis = new G4VisExecutive("quiet"); vis->Initialize();
+  if (ui) { // start an interactive UI session
+    ui->SessionStart(); delete ui; // clear memory at the end of UI session
+  } else { // run a macro file in batch mode
+    G4String cmd = "/control/execute ", macroFile = argv[1];
+    G4UImanager::GetUIpointer()->ApplyCommand(cmd + macroFile);
+  }
 
-	G4PhysListFactory factory;
-	run->SetUserInitialization(factory.ReferencePhysList());
-
-	run->SetUserInitialization(new Detector);
-
-	run->SetUserInitialization(new Action);
-
-	G4VisManager *vis = new G4VisExecutive("quiet"); vis->Initialize();
-
-	if (argc==1) { // interactive mode
-		G4UIExecutive ui(argc, argv);
-		ui.SessionStart();
-	} else { // batch mode
-		G4String command = "/control/execute ";
-		G4UImanager::GetUIpointer()->ApplyCommand(command+argv[1]);
-	}
-
-	delete vis; delete run;
+  delete vis;
+  delete run;
 }
